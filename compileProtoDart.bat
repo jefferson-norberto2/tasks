0aa85bcac9f76c533bc8bf72d03e01271b69183f
jefferson-norberto2 <jmn@cin.ufpe.br>
2024-08-15 09:45:14 -0300

    REFACTOR(proto): to proto 2

---
+++ compileProtoDart.bat
@@ -0,0 +1,14 @@
+cd lib\app\modules\home\submodules\tasks\infra\protobuf
+protoc --dart_out=. *.proto
+
+cd ..
+cd ..
+cd ..
+cd ..
+cd ..
+
+
+cd login\infra\protobuf
+protoc --dart_out=. *.proto
+
+cd ...
